c91c476d4cc29b2ca313e66ef5c6f58248250684
tonybargas8111-glitch <tonyvargas8111@gmail.com>
2026-08-25 14:30:42 -0700

    Pushed my 2nd migration. I didn't have my PK in my first table. 2nd migration update that.

---
+++ api/migrations/1787673050077_create-units.js
@@ -0,0 +1,48 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable("units", {
+        unit_id: {
+            type: "serial",
+            primaryKey: true,
+        },
+
+        unit_name: {
+            type: "varchar(50)",
+            notNull: true,
+        },
+
+        unit_type: {
+            type: "varchar(50)",
+            notNull: true,
+        },
+
+        uic: {
+            type: "varchar(20)",
+            notNull: true,
+            unique: true,
+        },
+
+        location: {
+            type: "varchar(100)",
+            notNull: true,
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable("units");
+};
---
+++ api/migrations/1787690983976_create-add-primary-key-to-units.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+   pgm.sql(`
+      ALTER TABLE units
+      ADD PRIMARY KEY (unit_id);
+   `)
+};
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+   pgm.sql(`
+      ALTER TABLE units
+      DROP CONSTRAINT units_pkey;
+   `);
+};
